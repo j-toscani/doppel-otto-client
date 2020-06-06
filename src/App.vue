7403a47590bf8f59c0027fc385b6483ab27d640a
j-toscani <julian_toscani@gmx.de>
2020-06-06 22:42:06 +0200

    add new colors

--- src/App.vue
+++ src/App.vue
@@ -10,41 +10,6 @@
 </template>
 
 <style>
-html {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  font-size: 16px;
-  box-sizing: inherit;
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  width: 100vw;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0px;
-  padding: 0px;
-  height: 100vh;
-  width: 100vw;
-}
-
-:root {
-  --color-background-main: 242, 236, 228;
-  --color-background-contrast: 2, 64, 64;
-  --color-interactive-dark: 191, 117, 90;
-  --color-interactive-bright: 242, 183, 5;
-  --color-font-bright: var(--color-background-main);
-  --color-font-dark: var(--color-background-contrast);
-}
-
 #app {
   background: rgb(var(--color-background-main));
 }
